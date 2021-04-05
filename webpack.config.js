a421c2873c5e6b020682920067e08840ddfe3983
Tony Field <tony@tonyfield.ca>
2021-04-05 15:51:53 -0400

    Build separate js and css files to match existing codebase.

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,44 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: [
-        // BMBF code
-        './app/static-assets/js/BMBF.js',
-        './app/static-assets/js/BMBF.tracking.js',
-        './app/static-assets/js/BMBF.foundation.js',
-        './app/static-assets/js/BMBF.social.js',
-        './app/static-assets/js/BMBF.registration.js',
-        './app/static-assets/js/BMBF.advertising.js',
-        './app/static-assets/js/BMBF.search.js',
-        './app/static-assets/js/BMBF.nlform.js',
-        './app/static-assets/js/BMBF.newsletter.js',
-        './app/static-assets/js/BMBF.profile.js',
-        './app/static-assets/js/BMBF.movie.js',
-        './app/static-assets/js/BMBF.movienight.js',
-
-        // Vendor code
-        './app/static-assets/js/vendor/foundation/foundation.magellan.js',
-        './app/static-assets/js/vendor/foundation/foundation.orbit.js',
-        './app/static-assets/js/vendor/ie/redirect.js',
-        './app/static-assets/js/vendor/jquery/jquery.fittext.custom.js',
-
-        // SASS
-        './app/static-assets/css/components.scss',
-        './app/static-assets/css/core.scss',
-        './app/static-assets/css/fonts.scss'
-    ],
+    entry: {
+        main: [
+            './app/static-assets/js/BMBF.js',
+            './app/static-assets/js/BMBF.tracking.js',
+            './app/static-assets/js/BMBF.foundation.js',
+            './app/static-assets/js/BMBF.social.js',
+            './app/static-assets/js/BMBF.registration.js',
+            './app/static-assets/js/BMBF.advertising.js',
+            './app/static-assets/js/BMBF.search.js',
+            './app/static-assets/js/BMBF.nlform.js',
+            './app/static-assets/js/BMBF.newsletter.js',
+            './app/static-assets/js/BMBF.profile.js',
+            './app/static-assets/js/BMBF.movie.js',
+            './app/static-assets/js/BMBF.movienight.js'
+        ],
+        vendor: [
+            './app/static-assets/js/vendor/foundation/foundation.magellan.js',
+            './app/static-assets/js/vendor/foundation/foundation.orbit.js',
+            './app/static-assets/js/vendor/ie/redirect.js',
+            './app/static-assets/js/vendor/jquery/jquery.fittext.custom.js'
+        ],
+        components: './app/static-assets/css/components.scss',
+        core: './app/static-assets/css/core.scss',
+        fonts: './app/static-assets/css/fonts.scss'
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
     mode: 'production',
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [
+        new RemoveEmptyScriptsPlugin(),
+        new MiniCssExtractPlugin()
+    ],
     module: {
         rules: [
             {
