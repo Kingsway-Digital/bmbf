5bc53796406940a13a89eeec14e1c61cda413a4f
Tony Field <tony@tonyfield.ca>
2021-04-05 11:29:58 -0400

    clean up helpers code.

--- helpers/helper-times.js
+++ helpers/helper-times.js
@@ -1,7 +1,9 @@
+'use strict';
 module.exports.times = function (n, block)  {
-	var accum = '';
-	for(var i = 0; i < n; ++i)
-		accum += block.fn(i);
+	let accum = '';
+	for(let i = 0; i < n; ++i) {
+        accum += block.fn(i);
+    }
 
 	return accum;
 };
